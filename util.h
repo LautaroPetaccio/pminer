9c23ad33d476a8e9ec91536091d4dfe77dc13ef9
Lautaro Petaccio <lausuper@gmail.com>
2015-06-27 13:29:16 -0300

    Added hex2bin

--- util.h
+++ util.h
@@ -8,8 +8,9 @@
 #include <unistd.h>
 
 void bin2hex(char *s, const unsigned char *p, const size_t len);
-uint64_t swap_uint64(uint64_t val);
-uint32_t swap_uint32(uint32_t val);
+uint64_t swap_uint64(const uint64_t val);
+uint32_t swap_uint32(const uint32_t val);
 void byte_swap(unsigned char* data, int len);
+bool hex2bin(unsigned char *dest, const char *hexstr, size_t len);
 
 #endif /* util.h */
