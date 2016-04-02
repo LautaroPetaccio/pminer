d2967fb80c11a6bb5f0bd78a607feccf6eedc020
Lautaro Petaccio <lausuper@gmail.com>
2016-04-02 18:57:40 -0300

    Project cleanup

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    puts("Hello world!\n");
-
-    return 0;
-}
--- sha.c
+++
@@ -1,7 +0,0 @@
-#include "sha256.h"
-
-int main() {
-	char hash[65];
-	sha256_hash("abc", 3, hash);
-	return 0;
-}
